c6c6a156f8de3b42d2ff4ff56ade4c8044866572
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2024-11-28 21:42:30 +0200

    Centered calendar lids on level

--- CalendarLid.cs
+++ CalendarLid.cs
@@ -4,7 +4,7 @@ using Jypeli;
 class CalendarLid : GameObject
 {
     public readonly int lidNumber;
-    private static readonly Image[] LidOpeningImages = Tools.LoadAnimation("LidOpening", 13);
+    private static readonly Image[] LidOpeningImages = Tools.LoadNumberedImages("LidOpening", 13);
     private static readonly SoundEffect CreakingDoor = Game.LoadSoundEffect("CreakingDoor.wav");
     public Animation LidOpening = new Animation(LidOpeningImages);
     public bool Opened { get; private set; }
@@ -12,19 +12,15 @@ class CalendarLid : GameObject
     public CalendarLid(double width, double height, int lidNumber) : base(width, height)
     {
         this.lidNumber = lidNumber;
-        // opened = false;
         PlaceLidNumber();
-        // this.Image = LidOpeningImages[0];
         this.Animation = LidOpening;
         this.Animation.StopOnLastFrame = true;
         this.Animation.Played += this.Animation.Stop;
         LidOpening.FPS = 10;
-        // LidOpening.Start();
     }
 
     private void PlaceLidNumber()
     {
-        // GameObject lidNumber = new GameObject(Width, Height);
         Label lidText = new Label(Width / 2, Height / 2, lidNumber + "");
         this.Add(lidText);
     }
@@ -42,6 +38,7 @@ class CalendarLid : GameObject
             }
         }
         // TODO Image for game
+        // TODO Keep track of opened lids
     }
 
     private bool CanYouOpen()
