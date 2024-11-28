f020f769cfadc83c0009800f6ef7711a7634569c
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2024-11-28 21:15:33 +0200

    Added documentation

--- Tools.cs
+++ Tools.cs
@@ -2,7 +2,6 @@ using Jypeli;
 
 public class Tools
 {
-
     /// <summary>
     /// Print mouse position on world in messagedisplay and console in format: "x, y"
     /// </summary>
@@ -15,12 +14,23 @@ public class Tools
         game.MessageDisplay.Add(mousePosStr);
     }
 
+
+    /// <summary>
+    /// Prints current mouse world position in Jypeli Messagedisplay and console
+    /// </summary>
+    /// <param name="game"></param>
     public static void PrintMousePositionOnClick(Game game)
     {
         string mousePos = FormatMousePosition(game);
         game.Mouse.Listen(MouseButton.Left, ButtonState.Pressed, () => PrintMousePosition(game), null);
     }
 
+
+    /// <summary>
+    /// Fetches current mouseposition in game and formats it
+    /// </summary>
+    /// <param name="game"></param>
+    /// <returns>Mouseposition in format "x, y"</returns>
     public static string FormatMousePosition(Game game)
     {
         Vector mousePos = game.Mouse.PositionOnWorld;
@@ -29,7 +39,14 @@ public class Tools
         return mousePosStr;
     }
 
-    public static Image[] LoadAnimation(string name, int frames)
+
+    /// <summary>
+    /// Loads png images numbered from 01 to nn
+    /// </summary>
+    /// <param name="name">Name of images without numbering</param>
+    /// <param name="frames">Number of images</param>
+    /// <returns>Array of Images</returns>
+    public static Image[] LoadNumberedImages(string name, int frames)
     {
         Image[] images = new Image[frames];
         for (int i = 0; i < frames; i++)
